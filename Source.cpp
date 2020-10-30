05092bd920e93d18bee6027a934e6cd8b5e1b253
HNephilim <erickdpiva@gmail.com>
2020-10-29 21:49:50 -0300

    fstream totalmente bugado

--- Source.cpp
+++ Source.cpp
@@ -1,18 +1,30 @@
 #include "timer.h"
+#include <string>
+
+
+std::ofstream rot::ProfilerTimer::m_EventOutput = std::ofstream("Testes.txt", std::ios::out | std::ios::in);
 
 
 int main()
-{
-	
+{	
+	rot::ProfilerTimer::Setup();
 	double a = 1;
+	uint64_t b = 0;
+
 
 	{
 		
-		rot::ProfilerTimer test("teste.json");
+		rot::ProfilerTimer pt("ForLoop", "ForLoop A");
+
 		for (int i = 0; i < 1020; i++)
 		{
-			a = a + i;
-			std::cout << a << std::endl;
+			a = a + i;			
+			rot::ProfilerTimer pt2((std::string("inner for loop") + std::to_string(i)).c_str(), "ForLoopB");
+			for (int j = 0; j < 451; j++)			
+			{
+
+				b = b * j;				
+			}
 		}
 		
 	}
